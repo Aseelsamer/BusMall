20d440ff957bd9b13f8723bb9c92a5419cc39209
Aseelsamer <assoul_08@hotmail.com>
2020-10-19 15:43:38 +0300

    chart

--- js/app.js
+++ js/app.js
@@ -96,6 +96,15 @@ function render (){
   middleImage.setAttribute('alt',middleMall.mallNmae);
   middleImage.setAttribute('title',middleMall.mallNmae);
 
+  var previousindex=[names.leftMall,names.rightMall,names.middleImage];
+  do{
+    names.leftMall= getrandomNumber();
+  }
+  while(previousindex)
+  
+  
+  
+
 }
 
 render();
@@ -144,5 +153,97 @@ function renderSummary() {
     var liE=document.createElement('li');
     ulE1.appendChild(liE);
     liE.textContent = `${Mall.all[i].mallNmae} has ${Mall.all[i].clicks} clicks and ${Mall.all[i].views} views`;
+    renderChart();
   }
 }
+
+var view=[];
+var numofClick=[];
+
+
+function renderChart(){
+  for(var i=0;i<Mall.all.length;i++){
+    view.push(Mall.all[i].views);
+  }
+  for(var i=0;i<Mall.all.length;i++){
+    numofClick.push(Mall.all[i].clicks);
+  }
+  var ctx = document.getElementById('myChart').getContext('2d');
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'bar', //pie, line
+    data: {
+      labels: ['bag',
+        'banana',
+        'bathroom',
+        'boots',
+        'breakfast',
+        'bubblegum',
+        'chair',
+        'cthulhu',
+        'dog-duck',
+        'dragon',
+        'pen',
+        'pet-sweep',
+        'scissors',
+        'shark',
+        'tauntaun',
+        'unicorn',
+        'water-can',
+        'sweep',
+        'usb'],
+      datasets: [{
+        label: '# of clicks',
+        data: numofClick,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 3
+      },
+      {
+        label: '# of Views',
+        data: view,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 3
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
+
